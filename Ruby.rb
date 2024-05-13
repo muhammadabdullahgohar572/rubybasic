30fcf5961acfcf306c141beac0da1103fd617a77
Abdullah <muhammadabdullahgohar572@gmail.com>
2024-05-13 05:40:57 -0700

    first commit

---
+++ Ruby.rb
@@ -0,0 +1,190 @@
+
+
+
+# a =5;
+
+# b=4;
+
+
+# if b==40  && b==40
+#     puts "if";
+
+# elsif a >3 and a===0
+
+# puts("eleees")
+# else
+#     puts "else"
+
+# end
+
+
+
+
+
+# i = 1
+
+# while i <= 10 do
+#   puts i
+#   i += 1
+# end
+
+# a=0
+
+# for a in 3..5
+# puts a
+# end
+
+
+
+
+
+
+
+
+# for a in a
+#     puts a
+
+# end
+
+
+# a=["abdullah","umer","Jhon","David", "Sarah"]
+# index =0
+# while index <= a.length do 
+#     puts a[index]
+#   index +=1
+# end
+
+
+# b=0;
+
+# while b<= a.length do
+
+#     puts a[b]
+
+#     b+=1
+# end
+
+
+
+# puts ("====================================")
+
+# b=["Liam","Olivia","Noah","Emma","Oliver","Ava",]
+
+# b.each do |element|
+#     puts (element)
+
+# end
+
+
+
+
+# for i in 0..5
+
+#     if i < 2 then
+#        break
+#        end
+# puts (i)
+# end
+
+
+# for a in 0..5
+#     if a <= 3 then
+#       next
+#     end
+#     puts "this is the value of #{a}"
+#   end
+  
+
+# b=["Liam","Olivia","Noah","Emma","Oliver","Ava",]
+
+# puts(b[0..3])
+
+# myname="abdullah";
+
+# puts(myname.capitalize)
+# puts(myname.downcase);
+# puts(myname.reverse);
+# puts(myname.length)
+# puts(myname[0..2])
+# def Name (a,b)
+#   puts("My name is #{a} and My father name is #{b}")
+# end
+
+# Name("abdullah","safdar")
+
+
+# This method prints the number of parameters passed and their values.
+#
+# @param test [Array] A splat parameter that takes multiple arguments
+
+
+
+
+
+
+# def Simplee(*test)
+#   puts("This is a number if parameter is #{test.length}")
+
+#   for i in 0..test.length 
+#     puts("this is value of parameter is #{test[i]}")
+#   end
+# end
+
+
+# Simplee "abdullah","safdar"
+
+
+# a="abdullah"
+
+# b=12345
+
+# puts("Enter a Email")
+
+
+# if a==="abdullah"
+
+# puts("Enter a password")
+
+#   if b===1234
+#     puts("login")    
+#   else puts("incoorct password")
+
+#   end
+#   else puts("incorrct Email")
+  
+# end
+
+# def Simple(*parameter)
+
+#   puts("This is a parameter length #{parameter.length}")
+
+
+#   for i in 0..parameter.length 
+#     puts("this is a paramter value #{parameter[i]}")
+#   end
+  
+# end  
+
+# Simple "abdullah" ,1234
+
+
+# def test 
+#   puts("this is  line no 1")
+#   yield "abdullah"
+
+#   puts("this is a line no 2")
+# end
+
+# test {|i|
+#   puts("this is a line 3 #{i}")
+# }
+
+# BEGIN {
+#   puts("This a Bignum Method")
+# }
+
+# END{
+#   puts("this is a End")
+# }
+
+# puts("this is a test")
